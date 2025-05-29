7988d3464c9bc7d5198a70a5d90303c3ac97113b
Vicente Venegas <vicente.venegas997@gmail.com>
2025-05-29 13:51:36 -0400

    Update pyo_test.py

--- pyo_test.py
+++ pyo_test.py
@@ -2,10 +2,18 @@ from pyo import *
 
 # 1. Start the server (output only)
 s = Server(audio="alsa", duplex=0).boot()
-s.start()
 
-# 2. Use a simple relative path:
-sf = SfPlayer("sound/S1.1.wav", speed=[1, 0.995], loop=True, mul=0.4).out()
+path = "sound/S1.1.wav" 
+
+# Loads the sound file in RAM. Beginning and ending points
+# can be controlled with "start" and "stop" arguments.
+t = SndTable(path)
+
+# Gets the frequency relative to the table length.
+freq = t.getRate()
+
+# Simple stereo looping playback (right channel is 180 degrees out-of-phase).
+osc = Osc(table=t, freq=freq, phase=[0, 0.5], mul=0.4).out()
 
 # 3. Enter the GUI loop
 s.gui(locals())
