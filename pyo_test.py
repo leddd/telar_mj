018b57a630874304838bd2ca8081413784ad8e6d
Vicente Venegas <vicente.venegas997@gmail.com>
2025-05-29 14:28:59 -0400

    Update pyo_test.py

--- pyo_test.py
+++ pyo_test.py
@@ -1,11 +1,9 @@
 from pyo import *
 import random
-import tkinter as tk
 
-# 1) Boot server
+# Boot server
 s = Server(duplex=0).boot().start()
 
-# 2) Sounds
 sounds = [
     "sound/S1.1.wav",
     "sound/S1.2.wav",
@@ -13,47 +11,24 @@ sounds = [
     "sound/S1.4.wav",
 ]
 
-# work here!!
-
-import tkinter as tk
-
-# 12-key mapping: A to J and W, E, T, Y, U for black keys
 key_map = {
-    'a': 0,
-    'w': 1,
-    's': 2,
-    'e': 3,
-    'd': 4,
-    'f': 5,
-    't': 6,
-    'g': 7,
-    'y': 8,
-    'h': 9,
-    'u': 10,
-    'j': 11
+    'a': 0, 'w': 1, 's': 2, 'e': 3,
+    'd': 4, 'f': 5, 't': 6, 'g': 7,
+    'y': 8, 'h': 9, 'u': 10, 'j': 11
 }
 
-base_freq = 261.63  # C4
+base_freq = 261.63
 
-def on_key(event):
-    key = event.char.lower()
+print("Press a key (a–j / w,e,t,y,u) and press Enter. Type 'q' to quit.")
+
+while True:
+    key = input("Key: ").lower()
+    if key == 'q':
+        break
     if key in key_map:
         semitone = key_map[key]
         pitch = 2 ** (semitone / 12)
         sample = random.choice(sounds)
         SfPlayer(sample, speed=pitch, loop=False, mul=0.5).out()
-
-# Simple Tkinter window
-root = tk.Tk()
-root.title("12-note Keyboard")
-root.geometry("300x100")
-label = tk.Label(root, text="Click here and press A–J / W–U keys")
-label.pack(pady=30)
-
-root.bind("<Key>", on_key)
-
-# Prevent GUI lockup
-root.after(10, lambda: None)
-
-# Start pyo GUI and tkinter
-s.gui(locals())
+    else:
+        print("Not mapped.")
